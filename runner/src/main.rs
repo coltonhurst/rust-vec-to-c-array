ce3bc21e5fced7b7545339b9a381f40b74c61d0a
Colton Hurst <colton@coltonhurst.com>
2025-04-29 09:54:23 -0400

    Add failing listPeopleOnTheBus() test

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -3,17 +3,38 @@ use core::ffi::c_int;
 
 unsafe extern "C" {
     fn test() -> c_int;
+    fn listPeopleOnTheBus(bus: *mut Bus, peopleCount: c_int);
 }
 
-// #[repr(C)]
-// pub struct Person {
+#[repr(C)]
+pub struct Person {
+    name: String,
+    age: i32,
+}
 
-// }
+#[repr(C)]
+pub struct Bus {
+    busName: String,
+    people: Vec<Person>,
+}
 
 fn main() {
-    println!("Hello, world!");
-    
+    // test() test
+    println!("test() test");
     unsafe {
         println!("test(): {:?}", test());
+    }
+
+    // listPeopleOnTheBus() test
+    println!("listPeopleOnTheBus() test");
+    let bobby = Person { name: String::from("Bobby"), age: 100 };
+    let susan = Person { name: String::from("Susan"), age: 99 };
+
+    let people: Vec<Person> = vec![bobby, susan];
+    let mut bus = Bus { busName: String::from("City Bus"),  people };
+    let bus_ptr: *mut Bus = &mut bus;
+
+    unsafe {
+        println!("test(): {:?}", listPeopleOnTheBus(bus_ptr, 2));
     }
 }
